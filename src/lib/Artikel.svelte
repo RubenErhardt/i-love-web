1b712e9dfa7d04992b698d25cf0aafa941940982
RubenErhardt <rubengamebattles@outlook.com>
2024-12-04 15:04:36 +0100

    .

---
+++ src/lib/Artikel.svelte
@@ -0,0 +1,10 @@
+<!-- <a href="/" class="article-link">
+    <article class="article">
+        <div class="img-container">
+        <img src="/" alt="">
+    </div>
+        <h2>Leervragen sprint </h2>
+        <p></p>
+        <p class="datum"></p>
+    </article>
+    </a> -->
